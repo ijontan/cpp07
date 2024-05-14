dc95bc35db3cc53fdb5e29328d2ffa33f345746e
ijontan <85871000+ijontan@users.noreply.github.com>
2024-05-14 16:20:04 +0800

    update ex00

--- ex00/includes/math.hpp
+++ ex00/includes/math.hpp
@@ -1,10 +1,38 @@
 #pragma once
 
-template <typename T>
-void swap(T &a, T &b);
+#include <string>
 
 template <typename T>
-T min(T a, T b);
+void swap(T &a, T &b)
+{
+	T tmp;
+
+	tmp = a;
+	a = b;
+	b = tmp;
+}
 
 template <typename T>
-T max(T a, T b);
+T min(T a, T b)
+{
+	if (a < b)
+		return a;
+	return b;
+}
+
+template <typename T>
+T max(T a, T b)
+{
+	if (a > b)
+		return a;
+	return b;
+}
+
+// template void swap<int>(int &a, int &b);
+// template void swap<std::string>(std::string &a, std::string &b);
+//
+// template int min<int>(int a, int b);
+// template std::string min<std::string>(std::string a, std::string b);
+//
+// template int max<int>(int a, int b);
+// template std::string max<std::string>(std::string a, std::string b);
